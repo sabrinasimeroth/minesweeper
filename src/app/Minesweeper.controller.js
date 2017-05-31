af79d94dee3e06d1511f5b90f56343ea9dc83e5b
Sabrina Simeroth <ses0607@gmail.com>
2017-05-31 13:30:11 -0700

    added mines

--- src/app/Minesweeper.controller.js
+++ src/app/Minesweeper.controller.js
@@ -9,13 +9,33 @@ function createMinefield() {
     for (var j = 0; j < 9; j++) {
       var spot = {};
       spot.isCovered = true;
+      spot.content = "empty";
       row.spots.push(spot);
     }
 
     minefield.rows.push(row);
   }
 
+  placeManyRadomMines(minefield);
+
   return minefield;
+}
+
+function getSpot(minefield, row, column) {
+    return minefield.rows[row].spots[column];
+}
+
+function placeRandomMine(minefield) {
+    var row = Math.round(Math.random() * 8);
+    var column = Math.round(Math.random() * 8);
+    var spot = getSpot(minefield, row, column);
+    spot.content = "mine";
+}
+
+function placeManyRadomMines(minefield){
+    for(var i = 0; i < 10; i++) {
+        placeRandomMine(minefield);
+    }
 }
 
 function MinesweeperController($scope) {
